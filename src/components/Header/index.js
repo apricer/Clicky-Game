7076d620b9e7cd5cb480d5841f291fba8d9ca793
Anthony Pricer <anthony.pricer@outlook.com>
2020-02-26 00:58:55 -0600

    Clicky-Game

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Header() {
     return (
         <header className="header">
-            <h1>Clicky Game!</h1>
+            <h1>Clicky Game - The Office Edition</h1>
             <h2>Click on an image to earn points, but don't click on any more than once!</h2>
         </header>
     );
